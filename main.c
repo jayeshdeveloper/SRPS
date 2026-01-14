af108ddcb0c56ad5143556da520ba93a7fe034aa
jayeshjaiswal <jayeshjaiswal.developer@gmail.com>
2026-01-14 22:31:49 +0530

    marks read operation success

--- main.c
+++ main.c
@@ -3,14 +3,17 @@
 #include <string.h>
 #include <math.h>
 
-FILE* openFile(char *);
-
 typedef struct {
     char* id;
     char* name;
-    int minor[5];
-    int major[5];
+    double minor[5];
+    double major[5];
 } Student;
+
+
+FILE* openFile(char *);
+void printArrOfStudent(Student* arrStudents[], int noOfStudents);
+void printStudent(Student* student);
 
 int main(){   
     FILE * fpInp  = openFile("input.txt");
@@ -22,29 +25,31 @@ int main(){
     Student* arrStudents[noOfStudents];
 
     for(int i=0; i<noOfStudents; i++){
-        Student* s = (Student*) malloc(sizeof(Student));
+        Student* student = (Student*) malloc(sizeof(Student));
         // char* id = "test"; 
         char *id = malloc(50 * sizeof(char));
         fscanf(fpInp, "%s", id);
-        printf("id is: %s \n", id);
+        student->id=id;
+        // printf("id is: %s \n", s->id);
 
         char *name = malloc(50 * sizeof(char));
         fscanf(fpInp, "%s", name);
-        printf("name is: %s \n", name);
+        // printf("name is: %s \n", name);
+        student->name=name;
 
         for(int i=0; i<5; i++){
-            fscanf(fpInp, "%d", &s->minor[i]);
+            fscanf(fpInp, "%lf", &student->minor[i]);
         }
 
+        // printf("%lf", s->minor[1]);
+
         for(int i=0; i<5; i++){
-            fscanf(fpInp, "%d", &s->major[i]);
+            fscanf(fpInp, "%lf", &student->major[i]);
         }
+        arrStudents[i]=student;
     }
    
-//    Student* st = arrStudents[0]; 
-//    char* name = &(st->id); 
-//    printf("name is: %s", name);
-   
+    printArrOfStudent(arrStudents, noOfStudents);
    
 }
 
@@ -59,5 +64,20 @@ FILE* openFile(char * fileName){
 
     return fpInp;
 }
+
+void printArrOfStudent(Student* arrStudents[], int noOfStudents){
+    for(int i=0; i<noOfStudents; i++){
+        Student* student = arrStudents[i];
+        printStudent(student);
+    }
+}
+
+void printStudent(Student* student){
+      printf("%-10s %-15s ", student->id, student->name);
+        for(int j=0; j<5; j++) printf("%-6.2lf ", student->minor[j]);
+        for(int j=0; j<5; j++) printf("%-6.2lf ", student->major[j]);
+        printf("\n");
+}
+
 
 
